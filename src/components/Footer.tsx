9be927016f85449ab67162aa3d862bcc1f5dfd7c
Meet Mehta <148692742+DerashreeM@users.noreply.github.com>
2025-10-23 13:18:14 +0530

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,4 @@
-import { Facebook, Twitter, Linkedin, Instagram } from "lucide-react";
+import { Facebook, Whatsapp, Maps, Instagram } from "lucide-react";
 
 const Footer = () => {
   const currentYear = new Date().getFullYear();
